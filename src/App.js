faec595484e62b0003fa0ae97bd3873dd3821087
Yurii <yurich3098@gmail.com>
2020-08-03 18:21:02 +0300

    add app structure and replace atributes class to className

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
-}
+
+const App = () => {
+	return (
+		<div>
+			<header className="header">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							<div className="logo">
+								<img src="img/logo.png" alt=""/>
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="menu">
+								<ul>
+									<li><a href="/">Home</a></li>
+									<li><a href="/">Products</a></li>
+									<li><a href="/">Payment</a></li>
+									<li><a href="/">Shipping</a></li>
+								</ul>
+							</div>
+						</div>
+						<div className="col-lg-3">
+							<div className="cart text-center">
+								<div className="products-count">0</div>
+								<div className="products-price">$500</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<main className="main">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							
+						</div>
+						<div className="col-lg-9">
+							
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer className="footer"></footer>
+		</div>
+	)
+};
 
 export default App;
