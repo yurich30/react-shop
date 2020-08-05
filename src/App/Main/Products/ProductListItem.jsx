e7c4f411900409a7e999f59384bff80765a9da0b
Yurii <yurich3098@gmail.com>
2020-08-05 19:21:45 +0300

    destracturing the ProductListItem

--- src/App/Main/Products/ProductListItem.jsx
+++ src/App/Main/Products/ProductListItem.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './product-list-item.css'
 
-const ProductsListItem = (props) => {
+const ProductsListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price
+}) => {
     return(
         <div className="product-list-item">
-            <h2 className="product-title">{props.name}</h2>
-            <div className="product-description">{props.description}</div>
-            <div className="product-features">Type: {props.type}</div>
-            <div className="product-features">Capacity: {props.capacity}GB</div>
-            <div classname="product-price">$ {props.price}</div>
+            <h2 className="product-title">{name}</h2>
+            <div className="product-description">{description}</div>
+            <div className="product-features">Type: {type}</div>
+            <div className="product-features">Capacity: {capacity}GB</div>
+            <div classname="product-price">$ {price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
     )
