74ed8172fad1b59f13bbd6e9b2ea35cecc7258d9
Yurii <yurich3098@gmail.com>
2021-01-17 23:09:46 +0200

    add Redux and create store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App'
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { productLikeReducer } from './store/ProductLikeReducer';
 
+const store = createStore(productLikeReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  ,
   document.getElementById('root')
 );
---
+++ src/store/ProductLikeReducer.jsx
@@ -0,0 +1,5 @@
+export const productLikeReducer = (state = {
+    1:true,
+    2:true
+}, action) => state;
+
