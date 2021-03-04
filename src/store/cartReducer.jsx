8eafab92dd319b0120d5a38a7053ae90b307ece9
Yurii <yurich3098@gmail.com>
2021-03-04 14:38:25 +0200

    add action types to cartReducer

--- src/store/cartReducer.jsx
+++ src/store/cartReducer.jsx
@@ -1,6 +1,26 @@
+import { omit } from "lodash"
+
 const cartReducer = (state = {
     1:3,
     2:3
-}, action) => state
+}, action) => {
+    switch(action.type){
+        case "ADD_PRODUCT_TO_CART" :
+            return{
+                ...state,
+                [action.id]: (state[action.id] || 0) + action.count 
+            }
+        case "REMOVE_PRODUCT_FROM_CART" :
+            return   omit(state,[action.id])
+              
+        case "CHANGE_PRODUCT_QUANTITY" :
+            return {
+                ...state,
+                [action.id]:action.quantity
+            }
+        default :
+            return state
+    }
+}
 
 export default cartReducer
